2a71759bf10338522b30056c86b3d64d8090dc22
Alex Siler <davidalexsiler@gmail.com>
2019-05-22 02:28:39 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -49,12 +49,12 @@ end
 def runner
   welcome
   
-#   card_total = initial_round
-#   until card_total >= 21
-#     hit?(card_total)
-#     display_card_total(card_total)
-#   end
-# end_game(card_total)
+  card_total = 0
+  until card_total >= 21
+    hit?(card_total)
+    display_card_total(card_total)
+  end
+end_game(card_total)
   
   
 end
